207997b2952e33b49736e266042f47143cf7ab58
Pavel Prichodko <prichodko.p@gmail.com>
2021-06-22 18:22:16 +0200

    feat: add design system packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
 import '~/styles/global.css'
 
+// import { SSRProvider } from '@react-aria/ssr'
+import { IdProvider } from '@radix-ui/react-id'
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <IdProvider>
+      <Component {...pageProps} />
+    </IdProvider>
+  )
 }
 
 export default App
