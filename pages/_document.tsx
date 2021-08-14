49f5c0183219ad3e530a7f85ccceddf238fe9350
Pavel Prichodko <prichodko.p@gmail.com>
2021-08-14 19:00:02 +0200

    feat: add stitches configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { getCssString } from 'stitches.config'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssString() }}
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
             rel="stylesheet"
