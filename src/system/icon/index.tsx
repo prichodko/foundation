8cde76b34fe69d176274692cf52dcea19b5e7bcb
Pavel Prichodko <prichodko.p@gmail.com>
2021-09-23 15:28:58 +0200

    feat: add Icon component

---
+++ src/system/icon/index.tsx
@@ -0,0 +1,14 @@
+import { AccessibleIcon } from '@radix-ui/react-accessible-icon'
+
+interface Props {
+  label: string
+  children: React.ReactNode
+}
+
+const Icon = (props: Props) => {
+  const { label, children } = props
+  return <AccessibleIcon label={label}>{children}</AccessibleIcon>
+}
+
+export { Icon }
+export type { Props as IconProps }
