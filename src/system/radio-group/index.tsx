0e9c9cea9a29d322d4835df5a29cfc3018ae56d6
Pavel Prichodko <prichodko.p@gmail.com>
2021-09-23 15:28:25 +0200

    feat: add RadioGroup component

--- src/system/radio-group/index.tsx
+++ src/system/radio-group/index.tsx
@@ -1,47 +1,57 @@
-import type {
-  RadioGroupOwnProps,
-  RadioGroupItemOwnProps,
-} from '@radix-ui/react-radio-group'
-import { ReactNode } from 'react'
+import { useId } from '@radix-ui/react-id'
 
-import { Label, LabelProps } from '~/system/label'
+import type { FormFieldProps } from '~/system/forms'
+import { useField } from '~/system/forms'
+import { Label } from '~/system/label'
 
 import { Root, Item, Indicator } from './style'
 
-interface Props extends RadioGroupOwnProps {
-  value: string
-  onChange: (value: string) => void
-  children: ReactNode
+interface Props extends FormFieldProps {
+  value?: string
+  onChange?: (value: string) => void
+  children: React.ReactNode
+  disabled?: boolean
+  invalid?: boolean
   required?: boolean
 }
 
 const RadioGroup = (props: Props) => {
   const { value, onChange, children, required } = props
 
+  const { field, fieldState, formState } = useField(props)
+
+  const invalid = props.invalid || fieldState?.invalid
+  const disabled = props.disabled || formState?.isSubmitting
+
   return (
     <Root
-      value={value}
-      onValueChange={onChange}
+      value={value ?? field?.value}
+      onValueChange={onChange ?? field?.onChange}
       required={required}
-      className="flex flex-col space-y-3"
+      aria-disabled={disabled}
+      aria-invalid={invalid}
+      className="space-y-3"
     >
       {children}
     </Root>
   )
 }
 
-interface RadioGroupItemProps extends RadioGroupItemOwnProps, LabelProps {
+interface ItemProps {
+  children: React.ReactNode
   value: string
   disabled?: boolean
   required?: boolean
 }
 
-const RadioGroupItem = (props: RadioGroupItemProps) => {
-  const { hideLabel, children, value, disabled, required } = props
+const RadioGroupItem = (props: ItemProps) => {
+  const { children, value, disabled, required } = props
+
+  const itemId = useId()
 
   return (
-    <Label label={children} hideLabel={hideLabel}>
-      <Item value={value} required={required} disabled={disabled}>
+    <Label label={children}>
+      <Item id={itemId} value={value} required={required} disabled={disabled}>
         <Indicator />
       </Item>
     </Label>
--- src/system/radio-group/style.tsx
+++ src/system/radio-group/style.tsx
@@ -1,7 +1,41 @@
 import * as RadioGroup from '@radix-ui/react-radio-group'
-import { styled } from 'stitches.config'
 
-export const Root = styled(RadioGroup.Root, {})
+import { styled, theme } from '~/styles/config'
+
+export const Root = styled(RadioGroup.Root, {
+  display: 'flex',
+
+  '&[aria-disabled="true"]': {
+    borderColor: theme.colors.red8,
+
+    '&:hover': {
+      borderColor: theme.colors.gray8,
+    },
+  },
+
+  '&[aria-invalid="true"]': {
+    borderColor: theme.colors.red8,
+
+    '&:hover': {
+      borderColor: theme.colors.gray8,
+    },
+  },
+
+  variants: {
+    orientation: {
+      vertical: {
+        flexDirection: 'column',
+      },
+      horizontal: {
+        flexDirection: 'row',
+      },
+    },
+  },
+
+  defaultVariants: {
+    orientation: 'vertical',
+  },
+})
 
 export const Item = styled(RadioGroup.Item, {
   display: 'inline-flex',
@@ -10,26 +44,21 @@ export const Item = styled(RadioGroup.Item, {
   height: 19,
   width: 19,
   flexShrink: 0,
-  border: '2px solid $gray8',
-  borderRadius: '$full',
-  color: '$gray1',
-  background: '$gray2',
-  transition: '$colors',
-  outline: 'none',
+  border: `2px solid ${theme.colors.gray8}`,
+  borderRadius: theme.radii.full,
+  color: theme.colors.gray1,
+  background: theme.colors.gray2,
+  // transition: $colors,
 
   '&:hover': {
-    borderColor: '$gray9',
-  },
-
-  '&:focus': {
-    ring: 2,
+    borderColor: theme.colors.gray9,
   },
 
   '&[aria-checked="true"]': {
-    borderColor: '$gray12',
+    borderColor: theme.colors.gray12,
 
     '&:hover': {
-      borderColor: '$gray12',
+      borderColor: theme.colors.gray12,
     },
   },
 })
@@ -37,6 +66,6 @@ export const Item = styled(RadioGroup.Item, {
 export const Indicator = styled(RadioGroup.Indicator, {
   width: 9,
   height: 9,
-  background: '$gray12',
-  borderRadius: '$full',
+  background: theme.colors.gray12,
+  borderRadius: theme.radii.full,
 })
