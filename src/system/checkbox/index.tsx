d750cc21673e0e48286aa9e62435d3970f9f9969
Pavel Prichodko <prichodko.p@gmail.com>
2021-09-23 15:27:12 +0200

    feat: add Checkbox component

--- src/system/checkbox/index.tsx
+++ src/system/checkbox/index.tsx
@@ -1,22 +1,42 @@
-import type { CheckboxOwnProps } from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
+import { useId } from '@radix-ui/react-id'
 
-import { Label, LabelProps } from '~/system/label'
+import type { FormFieldProps } from '~/system/forms'
+import { useField } from '~/system/forms'
+import { Label } from '~/system/label'
 
 import { Root, Indicator } from './style'
 
-interface Props extends LabelProps, CheckboxOwnProps {
+interface Props extends FormFieldProps {
+  checked?: boolean
+  onChange?: (checked: boolean) => void
   invalid?: boolean
   disabled?: boolean
   required?: boolean
+  children?: React.ReactNode
 }
 
 export const Checkbox = (props: Props) => {
-  const { children, disabled, invalid, ...checkboxProps } = props
+  const { children, checked, onChange, required } = props
+
+  const checkboxId = useId()
+
+  const { field, fieldState, formState } = useField(props)
+
+  const invalid = props.invalid || fieldState?.invalid
+  const disabled = props.disabled || formState?.isSubmitting
 
   return (
     <Label label={children}>
-      <Root {...checkboxProps} disabled={disabled} aria-invalid={invalid}>
+      <Root
+        id={checkboxId}
+        checked={checked ?? field?.value}
+        onCheckedChange={onChange ?? field?.onChange}
+        disabled={disabled}
+        required={required}
+        aria-invalid={invalid}
+        ref={field?.ref}
+      >
         <Indicator>
           <CheckIcon />
         </Indicator>
--- src/system/checkbox/style.tsx
+++ src/system/checkbox/style.tsx
@@ -1,49 +1,45 @@
 import * as Checkbox from '@radix-ui/react-checkbox'
-import { styled } from 'stitches.config'
+
+import { styled, theme } from '~/styles/config'
 
 export const Root = styled(Checkbox.Root, {
   height: 19,
   width: 19,
   flexShrink: 0,
-  border: '2px solid $gray8',
-  borderRadius: 4,
-  color: '$gray1',
-  background: '$gray2',
-  transition: '$colors',
-  outline: 'none',
+  borderRadius: theme.radii.element,
+  color: theme.colors.gray1,
+  background: theme.colors.gray2,
+  // transition: theme.colors.colors,
+  border: `2px solid ${theme.colors.gray7}`,
 
   '&:hover': {
-    borderColor: '$gray9',
-  },
-
-  '&:focus': {
-    ring: 2,
+    borderColor: theme.colors.gray8,
   },
 
   '&[aria-invalid="true"]': {
-    borderColor: '$red8',
+    borderColor: theme.colors.red8,
 
     '&:hover': {
-      borderColor: '$gray8',
+      borderColor: theme.colors.gray8,
     },
   },
 
   '&[aria-checked="true"]': {
-    background: '$gray12',
-    borderColor: '$gray12',
+    background: theme.colors.gray12,
+    borderColor: theme.colors.gray12,
 
     '&:hover': {
-      borderColor: '$gray12',
+      borderColor: theme.colors.gray12,
     },
   },
 
   '&:disabled': {
     cursor: 'not-allowed',
-    background: '$gray2',
-    borderColor: '$gray6',
+    background: theme.colors.gray2,
+    borderColor: theme.colors.gray6,
 
     '&:hover': {
-      borderColor: '$gray6',
+      borderColor: theme.colors.gray6,
     },
   },
 })
