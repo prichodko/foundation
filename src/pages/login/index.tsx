e8080e202279efd0ad0ca776c3dc23c66be28557
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-16 17:03:04 +0100

    feat: simplify login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import type { Page } from 'next'
+// import { signIn, useSession, signOut } from 'next-auth/react'
 
 import { Link } from '~/components/link'
 import { Button } from '~/system/button'
@@ -14,18 +15,20 @@ interface FormValues {
 }
 
 export const LoginPage: Page = () => {
+  // const { data: session } = useSession()
+
   const handleSubmit: FormSubmitHandler<FormValues> = async values => {
     console.log(values)
     return new Promise(resolve => setTimeout(resolve, 2000))
   }
 
   return (
-    <div className="flex items-center h-screen">
+    <div className="flex items-center min-h-screen">
       <div className="w-full max-w-sm mx-auto">
         <Heading align="center" size="32" className="mb-3">
           Welcome Back
         </Heading>
-        <Text align="center" color="secondary" className="mb-6">
+        <Text align="center" color="secondary" className="mb-10">
           {"Don't"} have an account?{' '}
           <Link href="/get-started" className="underline">
             Sign up for free
@@ -36,31 +39,32 @@ export const LoginPage: Page = () => {
           defaultValues={{ email: '', password: '' }}
           onSubmit={handleSubmit}
         >
-          <div className="grid gap-3 mb-6">
+          <div className="grid gap-4">
             <TextInput
               name="email"
-              label="Email"
+              // label="Email"
               placeholder="Email"
               autoComplete="username"
               rules={{ required: true, validate: value => !!value.trim() }}
             />
-            <TextInput
+            {/* <TextInput
               name="password"
               type="password"
               label="Password"
               placeholder="Password"
               autoComplete="current-password"
               rules={{ required: true }}
-            />
+            /> */}
+            <Button type="submit" width="full">
+              Continue
+            </Button>
           </div>
-          <Button type="submit" width="full">
-            Log In
-          </Button>
         </Form>
+
         <div className="flex flex-col items-center mt-6">
-          <Link href="/" className="hover:underline">
+          {/* <Link href="/" className="hover:underline">
             Forgot password?
-          </Link>
+          </Link> */}
           <Text size="12" align="center" color="secondary" className="mt-8">
             This site is protected by reCAPTCHA and the Google{' '}
             <a className="underline" href="https://policies.google.com/privacy">
