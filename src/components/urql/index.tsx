cf8666423c3e22b29dee8ef588c780c1c9c40058
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-16 16:48:04 +0100

    feat: add urql error logging

--- src/components/urql/index.tsx
+++ src/components/urql/index.tsx
@@ -1,7 +1,50 @@
-import { createClient, Provider } from 'urql'
+import { devtoolsExchange } from '@urql/devtools'
+import {
+  createClient,
+  Provider,
+  errorExchange,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+} from 'urql'
 
 const urqlClient = createClient({
   url: 'http://localhost:3000/api/graphql',
+  exchanges: [
+    devtoolsExchange,
+    dedupExchange,
+    cacheExchange,
+    errorExchange({
+      onError(error) {
+        const { graphQLErrors, networkError } = error
+        for (let graphQLError of graphQLErrors) {
+          console.error(graphQLError)
+          // switch (err.extensions.code) {
+          //   // Apollo Server sets code to UNAUTHENTICATED
+          //   // when an AuthenticationError is thrown in a resolver
+          //   case 'UNAUTHENTICATED':
+
+          //     // Modify the operation context with a new token
+          //     const oldHeaders = operation.getContext().headers;
+          //     operation.setContext({
+          //       headers: {
+          //         ...oldHeaders,
+          //         authorization: getNewToken(),
+          //       },
+          //     });
+          //     // Retry the request, returning the new observable
+          //     return forward(operation);
+          // }
+          // }
+        }
+
+        if (networkError) {
+          console.error(`[Network Error]:`, networkError)
+        }
+      },
+    }),
+    fetchExchange,
+  ],
 })
 
 interface Props {
