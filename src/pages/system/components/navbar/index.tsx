8e91283609559fd240109d4bfec4571e66a2eb3b
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-16 17:02:16 +0100

    refactor: rename system page

---
+++ src/pages/system/components/navbar/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from '~/components/link'
+import { Button } from '~/system/button'
+
+import { ThemeSwitcher } from './components/theme-switcher'
+
+interface Props {}
+
+const Logo = () => {
+  return <div className="font-mono ">workverse</div>
+}
+
+export const Navbar = ({}: Props) => {
+  return (
+    <div className="fixed top-0 left-0 right-0 z-20 flex items-center justify-between px-4 py-2 border-b backdrop-filter backdrop-blur">
+      <ThemeSwitcher />
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <div className="grid grid-flow-col gap-2">
+        <Button href="/login" variant="minimal">
+          Sign In
+        </Button>
+        <Button href="/dashboard/new">New Post</Button>
+      </div>
+    </div>
+  )
+}
