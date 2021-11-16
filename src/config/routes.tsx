0f7459f7bcbd2bba49c9b0929f6b5a1847d452b6
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-16 17:01:36 +0100

    feat: add typed routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,1 +1,31 @@
-export type Routes = '/' | '/login' | '/get-started'
+import type { UrlObject } from 'url'
+
+type ExtractRouteParams<T extends string> = string extends T
+  ? Record<string, string>
+  : T extends `${infer _Start}[${infer Param}]/${infer Rest}`
+  ? { [k in Param | keyof ExtractRouteParams<Rest>]: string }
+  : T extends `${infer _Start}[${infer Param}]`
+  ? { [k in Param]: string }
+  : {}
+
+type DynamicRoute<Path extends string> = Omit<UrlObject, 'query'> & {
+  pathname: Path
+  query: ExtractRouteParams<Path>
+}
+
+export type Routes =
+  | '/'
+  | '/sign-up'
+  | '/login'
+  | '/get-started'
+  | '/jobs/dao'
+  | '/jobs/defi'
+  | '/jobs/gaming'
+  | '/jobs/nft'
+  | '/companies'
+  | DynamicRoute<'/[slug]'>
+  | '/dashboard'
+  | '/dashboard/new'
+  | DynamicRoute<'/dashboard/[slug]'>
+  | DynamicRoute<'/dashboard/[slug]/[id]'>
+  | DynamicRoute<'/dashboard/jobs/[id]'>
