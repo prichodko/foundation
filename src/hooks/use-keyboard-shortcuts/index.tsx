8c775094004dbcf2753e823256103c6e2e5b7a98
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-16 17:01:48 +0100

    feat: update global shortcuts

--- src/hooks/use-keyboard-shortcuts/index.tsx
+++ src/hooks/use-keyboard-shortcuts/index.tsx
@@ -4,7 +4,14 @@ import tinykeys from 'tinykeys'
 
 export const useKeyboardShortcuts = () => {
   useEffect(() => {
-    const unsubscribe = tinykeys(window, {})
+    const unsubscribe = tinykeys(window, {
+      '$mod+/': event => {
+        console.log('here', document.getElementById('search'))
+        event.preventDefault()
+
+        document.getElementById('search')?.focus()
+      },
+    })
     return () => {
       unsubscribe()
     }
