86a147ec1a8a22bcb718ca4420d233f9d696b446
Pavel Prichodko <prichodko.p@gmail.com>
2021-11-30 01:15:03 +0100

    fix(api): slack webhook urls

--- src/api/services/slack.ts
+++ src/api/services/slack.ts
@@ -1,31 +1,25 @@
 const sales = async (message: string) => {
-  await fetch(
-    'https://hooks.slack.com/services/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',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        text: message,
-      }),
-    }
-  )
+  await fetch(process.env.SLACK_SALES_WEBHOOK_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: message,
+    }),
+  })
 }
 
 const feedback = async (message: string) => {
-  await fetch(
-    'https://hooks.slack.com/services/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',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        text: message,
-      }),
-    }
-  )
+  await fetch(process.env.SLACK_FEEDBACK_WEBHOOK_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: message,
+    }),
+  })
 }
 
 export const slack = {
