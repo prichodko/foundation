2a7b738967a9667228f42b3b08bdde437ecaa279
Pavel Prichodko <prichodko.p@gmail.com>
2021-12-05 17:16:43 +0100

    fix: support base url env variable

---
+++ src/config/env.tsx
@@ -0,0 +1,3 @@
+export const env = {
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000',
+}
--- src/hooks/use-callback-url/index.tsx
+++ src/hooks/use-callback-url/index.tsx
@@ -1,7 +1,8 @@
+import { env } from '~/config/env'
 import type { Routes } from '~/config/routes'
 
 import { useUrlQuery } from '../use-url-query'
 
 export const useCallbackUrl = (route: Routes): string => {
-  return useUrlQuery('callbackUrl') ?? `${process.env.NEXTAUTH_URL}${route}`
+  return useUrlQuery('callbackUrl') ?? `${env.baseUrl}${route}`
 }
