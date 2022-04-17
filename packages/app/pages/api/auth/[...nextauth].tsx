1cd8383f88db9b6b554dcba5ffab50fa741e409c
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-17 15:16:53 +0200

    feat(app): update next-auth handler

--- packages/app/pages/api/auth/[...nextauth].tsx
+++ packages/app/pages/api/auth/[...nextauth].tsx
@@ -1,40 +1,77 @@
+import { env, prisma } from '@example/api'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '@example/api'
 import NextAuth from 'next-auth'
-// import EmailProvider from 'next-auth/providers/email'
-import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: 'Foundation <no-reply@example.com>',
-    // }),
-  ],
+import type { NextApiHandler } from 'next'
 
-  callbacks: {},
+const handler: NextApiHandler = (req, res) => {
+  return NextAuth(req, res, {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: env.google.clientId,
+        clientSecret: env.google.clientSecret,
+      }),
+      // {
+      //   id: 'email',
+      //   type: 'email',
+      //   name: 'Email',
+      //   sendVerificationRequest: async ({ identifier: email, url }) => {
+      //     // TODO: Send verification email
+      //   },
+      //   server: {},
+      //   options: {},
+      // },
+    ],
 
-  pages: {
-    //   signIn: '/login',
-    //   signOut: '/',
-    //   error: '/login', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // (used for check email message)
-    // newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
-  },
+    callbacks: {
+      async signIn(options) {
+        const { user, email } = options
 
-  session: {
-    jwt: true,
-  },
-  jwt: {
-    // A secret to use for key generation - you should set this explicitly
-    // Defaults to NextAuth.js secret if not explicitly specified.
-    // This is used to generate the actual signingKey and produces a warning
-    // message if not defined explicitly.
-    secret: process.env.SECRET,
-  },
-})
+        if (email?.verificationRequest) {
+          const type = req.query.type as 'login' | 'signup'
+
+          const existingUser = await prisma.user.findFirst({
+            where: {
+              id: user.id,
+            },
+          })
+
+          if (type === 'login' && !existingUser) {
+            throw new Error('User not found')
+          }
+
+          if (type === 'signup' && existingUser) {
+            throw new Error('User already exists')
+          }
+        }
+
+        return true
+      },
+    },
+
+    pages: {
+      signIn: '/login',
+      //   signOut: '/',
+      error: '/login', // Error code passed in query string as ?error=
+      verifyRequest: '/verify', // (used for check email message)
+      newUser: '/dashboard', // New users will be directed here on first sign in (leave the property out if not of interest)
+    },
+
+    session: {
+      strategy: 'jwt',
+    },
+    jwt: {
+      // A secret to use for key generation - you should set this explicitly
+      // Defaults to NextAuth.js secret if not explicitly specified.
+      // This is used to generate the actual signingKey and produces a warning
+      // message if not defined explicitly.
+      // secret: process.env.SECRET,
+    },
+    secret: env.auth.secret,
+    // debug: true,
+  })
+}
+
+export default handler
